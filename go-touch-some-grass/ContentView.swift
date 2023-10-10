fb21279b5584dd06c194aec0a19c0d07a5443461
Faexit <mrfaexit@gmail.com>
2023-10-10 17:43:00 +0200

    Initial Commit

---
+++ go-touch-some-grass/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  go-touch-some-grass
+//
+//  Created by Andreas Parsch on 10.10.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ go-touch-some-grass/go_touch_some_grassApp.swift
@@ -0,0 +1,17 @@
+//
+//  go_touch_some_grassApp.swift
+//  go-touch-some-grass
+//
+//  Created by Andreas Parsch on 10.10.23.
+//
+
+import SwiftUI
+
+@main
+struct go_touch_some_grassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
